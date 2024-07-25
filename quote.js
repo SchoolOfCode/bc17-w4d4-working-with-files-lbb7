f59fc2f2950205c0965168b8c3afb84e46a68d17
Lana Maugé <lanamauge@gmail.com>
2024-07-25 12:32:05 +0100

    ticket 2b attempt

--- quote.js
+++ quote.js
@@ -1,11 +1,21 @@
 import fs from "node:fs/promises";
+import { readFile } from 'fs/promises';
 import { v4 as uuidv4 } from "uuid";
 
 const fileName = "quotes.json";
 
 export async function getQuotes() {
-
+    try {
+        const data = await readFile(fileName, 'utf8');
+        const quotes = JSON.parse(data);
+        return quotes; // Return the parsed quotes
+    } catch (error) {
+        console.error('Error reading file:', error);
+        return [];
+    }
+    
 }
+
 
 export async function addQuote(quoteText) {
 
